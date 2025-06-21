9b5f05dd7464b4709b9cb366ffddef4174fe1f80
Ganesh Kumar <rjeekay24@gmail.com>
2025-06-22 00:25:14 +0530

    Updated Set and Tuple

---
+++ set/set_operations.py
@@ -0,0 +1,18 @@
+city1 = {"chennai", "mumbai", "hyderabad"}
+city2 = {"kochi", "bengaluru", "tuticorin"}
+
+# print(city1.union(city2))
+
+# print(city1.intersection(city2))
+
+# print(city1.difference(city2))
+
+# city1.add("madhya pradesh")
+# print(city1)
+
+# city1.remove("chennai")
+# city1.add("salem")
+# print(city1)
+
+# city1.discard("erode")
+# print(city1)
---
+++ tuple/tuple_operations.py
@@ -0,0 +1,13 @@
+trip = ("ubergo", "chennai", 500.50, "pattaya")
+
+# print(trip)
+
+# print(trip[2])
+
+# for trips in trip:
+#     print(trips)
+
+# print(len(trip))
+
+# print(trip.count(500.50))
+# print(trip.index("ubergo"))
